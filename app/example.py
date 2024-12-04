088ec102a33221229bacc2b05d9415855564c4c9
aartintelligent <aurelien.andre@aartintelligent.fr>
2024-12-04 21:55:04 +0100

    initial commit

--- app/example.py
+++ app/example.py
@@ -1,35 +1,60 @@
+import json
+
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 
-spark = SparkSession.builder \
+spark_session = SparkSession.builder \
     .appName('Save DataFrame to database') \
-    .config('spark.jars', '/opt/spark/jars/postgresql-42.7.4.jar') \
     .getOrCreate()
 
-data = [
-    (1, 'Alice'     , 28),
-    (2, 'Bob'       , 35),
-    (3, 'Charlie'   , 61)
-]
-
-schema = StructType([
-    StructField('id', IntegerType(), True),
-    StructField('name', StringType(), True),
-    StructField('age', IntegerType(), True)
-])
-
-df = spark.createDataFrame(data, schema)
-
-db_url = 'jdbc:postgresql://database:5432/postgres'
-
-db_properties = {
-    'user'      : 'spark',
-    'password'  : 'password',
-    'driver'    : 'org.postgresql.Driver'
+schemas = {
+    'example': StructType([
+        StructField('id', IntegerType()),
+        StructField('name', StringType()),
+        StructField('age', IntegerType())
+    ])
 }
 
-table_name = 'example_table'
+schemas_data = [(name, schema.json()) for name, schema in schemas.items()]
 
-df.write.jdbc(url=db_url, table=table_name, mode='overwrite', properties=db_properties)
+schemas_df = (
+    spark_session.
+    createDataFrame(schemas_data, [
+        'resource',
+        'json'
+    ])
+)
 
-spark.stop()
+schemas_df.write.jdbc(
+    url='jdbc:postgresql://database:5432/postgres',
+    table='_schemas',
+    mode='overwrite',
+    properties={
+        'user': 'spark',
+        'password': 'password',
+        'driver': 'org.postgresql.Driver'
+    }
+)
+
+schemas = {row['resource']: StructType.fromJson(json.loads(row['json'])) for row in schemas_df.collect()}
+
+exemple_data = [
+    (1, 'Alice', 28),
+    (2, 'Bob', 35),
+    (3, 'Charlie', 61)
+]
+
+df = spark_session.createDataFrame(exemple_data, schemas['example'])
+
+df.write.jdbc(
+    url='jdbc:postgresql://database:5432/postgres',
+    table='table_example',
+    mode='overwrite',
+    properties={
+        'user': 'spark',
+        'password': 'password',
+        'driver': 'org.postgresql.Driver'
+    }
+)
+
+spark_session.stop()
